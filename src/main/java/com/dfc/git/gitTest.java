da41d7cc394dfc6dfe26cb3a402338fdb17cd00f
Vgtbs.d <2984033365@qq.com>
2023-03-06 08:31:21 +0800

    hot-fix commit1

--- src/main/java/com/dfc/git/gitTest.java
+++ src/main/java/com/dfc/git/gitTest.java
@@ -13,5 +13,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("你好，git!");
         System.out.println("你好，git2!");
+        System.out.println("你好，git3!");
     }
 }
