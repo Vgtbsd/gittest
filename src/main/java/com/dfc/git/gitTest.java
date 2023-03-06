af35c56b91dc622b870e865ccce73c4501eb5bd5
Vgtbs.d <2984033365@qq.com>
2023-03-06 08:33:45 +0800

    master commit1

--- src/main/java/com/dfc/git/gitTest.java
+++ src/main/java/com/dfc/git/gitTest.java
@@ -14,5 +14,6 @@ public class gitTest {
         System.out.println("你好，git!");
         System.out.println("你好，git2!");
         System.out.println("你好，git3!");
+        System.out.println("我是 master");
     }
 }
