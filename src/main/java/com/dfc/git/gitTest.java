997aee117d5bf6191c704f9d29245dd7654c7a9f
Vgtbs.d <2984033365@qq.com>
2023-03-06 08:08:06 +0800

    first commit

---
+++ src/main/java/com/dfc/git/gitTest.java
@@ -0,0 +1,16 @@
+package com.dfc.git;
+
+/**
+ * ClassName:gitTest
+ * Package:com.dfc.git
+ * Description:
+ *
+ * @Author 代富成
+ * @Create 2023/3/6 - 8:04
+ * @Version v1.0
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("你好，git!");
+    }
+}
